c38e076a5ecf364dd3adbec3d4926ff2c5d08da7
Dmitry <dm.rubashko@gmail.com>
2021-09-12 01:25:26 +0300

    + few tasks

--- index.js
+++ index.js
@@ -76,24 +76,15 @@ const users = [
 // }
 
 
-
-
-
-
-
-
-
-
-
 // console.log(getSortUsers(users))
     
 // console.log(users[4].gender)
 
 
 // task 4 
-const initialArray = [1, 'Hello', 3, 'Bye'];
-const sortedArray = (randomArray) => {
-    return randomArray.map((item, index) =>({
+const initialArray = [1, 'Hello', 3, 'Bye', 'WORLD'];
+const sortedArray = (unsortedArray) => {
+    return unsortedArray.map((item, index) =>({
         elementValue: item,
         elementType: typeof (item),
         elementIndex: index
@@ -101,3 +92,68 @@ const sortedArray = (randomArray) => {
 }
 
 console.log (sortedArray(initialArray))
+
+// task 5 
+const isPalindrome = (word) => {
+    for (let i = 0; i <= Math.floor(word.length/2); i++) {
+        if (word[i].toLowerCase() !== word[word.length-i-1].toLowerCase()) {
+            return false;
+        }; 
+    };
+    return true;
+}
+console.log (isPalindrome('ШАлАш'));
+console.log (isPalindrome('привет'));
+
+// task 7
+
+const newReleases = [
+    {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    ur: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: []
+    }, 
+    
+    {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }]
+    }, 
+    
+    {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: []
+    }, 
+    
+    {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }]
+   }
+];
+
+const releaseId = (releases) => {
+    const result = [];
+    for (let i = 0; i < releases.length; i++) {
+        if (Number(releases[i].rating) === 5) {
+            result.push(releases[i].id);
+        };   
+    };
+    return result;
+}
+  
+console.log(releaseId(newReleases));
+
